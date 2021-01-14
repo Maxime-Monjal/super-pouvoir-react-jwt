afa60a6c584d2320e035c8a098c3b72b3e2763c9
loicbdev <loicbodolec@msn.com>
2021-01-14 14:23:49 +0100

    pull dev remote

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/commons/Header/Header";
 import Footer from "./components/commons/Footer/Footer";
-import "./App.css";
-<<<<<<< HEAD
-// import ProductsList from "./components/views/ProductsList";
 import Power from "./components/commons/Power/Power";
-=======
->>>>>>> 96c2af5d34dba921ffffa47d0365ffe5220711c8
+import "./App.css";
 
 function App() {
   return (
