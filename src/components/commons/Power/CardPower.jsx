0ed156887c9126f61212b59a75e86287d426d8bf
loicbdev <loicbodolec@msn.com>
2021-01-14 18:43:11 +0100

    update CSS mobile

--- src/components/commons/Power/CardPower.jsx
+++ src/components/commons/Power/CardPower.jsx
@@ -3,15 +3,15 @@ import PropTypes from "prop-types";
 import style from "./CardPower.module.css";
 
 function CardPower({ power }) {
-  const { title, prix, image, description } = power;
+  const { title, prix, picture, contenu_produit } = power;
   return (
     <div className={style.CardPower}>
       <div className={style.container}>
         <h3 className={style.title}>{title}</h3>
         <span className={style.price}>{prix} euros</span>
         <div className={style.containerImg}>
-          <img className={style.img} src={image} alt={title} />
-          <p className={style.description}>{description}</p>
+          <img className={style.img} src={picture} alt={title} />
+          <p className={style.description}>{contenu_produit}</p>
         </div>
         <button className={style.button} type="submit">
           Ajouter
@@ -24,9 +24,9 @@ function CardPower({ power }) {
 CardPower.propTypes = {
   power: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
+    contenu_produit: PropTypes.string.isRequired,
     prix: PropTypes.number.isRequired,
-    image: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
   }).isRequired,
 };
 
