3ae9c585e6a90416f01e5ffb6a238fc55dedd45f
Aymeric-Lancien <aymericlancien12@gmail.com>
2021-01-14 12:07:14 +0100

    header, router and font

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./fonts/Aquawax Light Trial.woff";
-import "./fonts/Aquawax Medium Trial.woff";
-import "./fonts/Aquawax Black Trial.woff";
-import "./fonts/AVENGEANCE HEROIC AVENGER.ttf";
-import "./fonts/AVENGEANCE HEROIC AVENGER BD.ttf";
-import "./fonts/AVENGEANCE HEROIC AVENGER BI.ttf";
-import "./fonts/AVENGEANCE HEROIC AVENGER AT.ttf";
+import "./main.css";
+import "./index.css";
 
 import App from "./App";
 
