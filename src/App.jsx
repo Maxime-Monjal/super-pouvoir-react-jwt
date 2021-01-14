0ea833139eb4a6b97479eae4e05ea951178f096f
NIcolasBonamy <n.bonamy@live.fr>
2021-01-14 14:27:00 +0100

    add cart components

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/commons/Header";
 import "./App.css";
 import Power from "./components/commons/Power/Power";
+import Cart from "./components/views/Cart/Cart";
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route exact path="/" />
-        <Route path="/panier" />
+        <Route exact path="/" component={Power} />
+        <Route path="/panier" component={Cart} />
       </Switch>
     </Router>
   );
---
+++ src/components/views/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../commons/Header";
+import CardPowerCart from "./CardPowerCart";
+
+function Cart() {
+  return (
+    <div>
+      <Header />
+      <CardPowerCart />
+    </div>
+  );
+}
+
+export default Cart;
