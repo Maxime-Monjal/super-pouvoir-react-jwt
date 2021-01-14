ab2ead54ec36885f6849bf91c782d8e018a3a7e6
loicbdev <loicbodolec@msn.com>
2021-01-14 16:35:41 +0100

    update

--- src/components/views/Cart/Cart.jsx
+++ src/components/views/Cart/Cart.jsx
@@ -1,15 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import CardPowerCart from "./CardPowerCart";
-=======
 import CardPowerCart from "../../commons/Power/CardPowerCart";
 import style from "./Cart.module.css";
->>>>>>> becf71e93ea7615cc1329778e83870150746d4b6
-=======
-import CardPowerCart from "../../commons/Power/CardPowerCart";
-import style from "./Cart.module.css";
->>>>>>> b1de2973f69f5009c8d674c131268b087ca21deb
 
 function Cart() {
   return (
