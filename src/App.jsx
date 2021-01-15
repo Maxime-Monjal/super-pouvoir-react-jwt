18d3dab84642389369e488d86bc3d50984bf0226
NIcolasBonamy <n.bonamy@live.fr>
2021-01-15 12:17:41 +0100

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/views/Home/Home";
 import Product from "./components/views/Product";
 import Header from "./components/commons/Header/Header";
 import Footer from "./components/commons/Footer/Footer";
-import Home from "./components/views/Home/Home";
 import Cart from "./components/views/Cart/Cart";
 import Page404 from "./components/views/Page404/Page404";
 
@@ -18,7 +18,7 @@ function App() {
         <Route exact path="/produits/:categorie" component={Home} />
         <Route path="/product/:power" component={Product} />
         <Route path="/panier" component={Cart} />
-        <Route path="/404" component={Page404} />
+        <Route path="/:all" component={Page404} />
       </Switch>
       <Footer />
     </Router>
